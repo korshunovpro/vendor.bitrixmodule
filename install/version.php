736383b4440e66c36c5371a4443b531f00c231f0
sergey <sergey@korshunov.pro>
2019-01-28 13:26:34 +0300

    add version.php

---
+++ install/version.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Bitrix module skeleton
+ *
+ * @author Sergey Korshunov <sergey@korshunov.pro>
+ * @license	https://opensource.org/licenses/MIT	MIT License
+ */
+
+$arModuleVersion = [
+    'VERSION' => '0.0.0',
+    'VERSION_DATE' => '2019-01-28 07:45:00'
+];
