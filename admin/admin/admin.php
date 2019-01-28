6b9cac8fa7cbdbe301952624c5714b336013079e
sergey <sergey@korshunov.pro>
2019-01-28 13:56:04 +0300

    module admin page

--- admin/admin/admin.php
+++ admin/admin/admin.php
@@ -5,3 +5,18 @@
  * @author Sergey Korshunov <sergey@korshunov.pro>
  * @license	https://opensource.org/licenses/MIT	MIT License
  */
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_admin_before.php");
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_admin_after.php");
+
+\Bitrix\Main\Loader::includeModule('vendor.bitrixmodule');
+
+use Bitrix\Main\Localization\Loc;
+
+Loc::loadMessages(__FILE__);
+
+$APPLICATION->SetTitle(Loc::getMessage('VENDOR_PREFIX_PAGE_EDIT_TITLE'));
+
+// html, js & php code
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/epilog_admin.php");
---
+++ lang/ru/admin/admin/admin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Bitrix module skeleton
+ *
+ * @author Sergey Korshunov <sergey@korshunov.pro>
+ * @license	https://opensource.org/licenses/MIT	MIT License
+ */
+
+$MESS['VENDOR_PREFIX_PAGE_EDIT_TITLE'] = 'Module admin page';
