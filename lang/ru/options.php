e7db5d4b5c0a9b9aaf9f1c4d1e726d4779742f5e
sergey <sergey@korshunov.pro>
2019-01-29 05:46:44 +0300

    module options

---
+++ lang/ru/options.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Bitrix module skeleton
+ *
+ * @author Sergey Korshunov <sergey@korshunov.pro>
+ * @license	https://opensource.org/licenses/MIT	MIT License
+ */
+
+$MESS['VENDOR_PREFIX_OPT_HTML_NAME']        = 'Текст с HTML разметкой';
+$MESS['VENDOR_PREFIX_OPT_HTML_DESC']        = 'Описание';
+$MESS['VENDOR_PREFIX_OPT_TEXT_NAME']        = 'Просто текст';
+$MESS['VENDOR_PREFIX_OPT_TEXT_DESC']        = 'Тут описание';
+$MESS['VENDOR_PREFIX_OPT_TEXTAREA_NAME']    = 'Тектовое поле textarea';
+$MESS['VENDOR_PREFIX_OPT_INPUT_NAME']       = 'Тектовое поле input';
+$MESS['VENDOR_PREFIX_OPT_ADDITIONAL']       = 'Дополнительные настройки';
+$MESS['VENDOR_PREFIX_OPT_MULTISELECT_NAME'] = 'Список множественного выбора';
+$MESS['VENDOR_PREFIX_OPT_SELECT_NAME']      = 'Выпадающий список';
+$MESS['VENDOR_PREFIX_OPT_CHECKBOX_NAME']    = 'Чекбокс';
+$MESS['VENDOR_PREFIX_MAIN_TAB_TITLE_SET']   = 'Настройка параметров модуля';
+$MESS['VENDOR_PREFIX_OPT_MAIN']             = 'Основные настройки';
+$MESS['VENDOR_PREFIX_INFO']                 = 'Некоторые заметки';
